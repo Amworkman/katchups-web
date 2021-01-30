91e578c7632a0cb0295073e5e7b1853d4b63402d
Adam Workman <adammworkman25@gmail.com>
2021-01-29 19:09:52 -0500

    create controlled sign up form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import './Login.css';
 
-const Login = () => {
+  const initialState = {
+    name: "",
+    location: "",
+    email: "",
+    password: "",
+    password_confirmation: "",    
+  }
+
+  function reducer(state, { name, value }) {
+    return {
+      ...state,
+      [name]: value
+    }
+  }
+
+  function Login() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleChange = (e) => {
+      dispatch({name: e.target.name, value: e.target.value})
+    } 
+
+    const { name, location, email, password, password_confirmation } = state  
+
   return (
-    <div>
-      
+    <div className="login-card">
+      <div className="form" fade-in-element>
+        <form>
+          <input type="text" name="name" className="input-box" value={name} onChange={handleChange} placeholder="Full Name" />
+          <input type="location" id="user-location-input" name="location" className="input-box"  value={location}  onChange={handleChange} placeholder="Location (ex. Atlanta, GA)" />
+          <input type="email" name="email" className="input-box"  value={email}  onChange={handleChange} placeholder="Email" />
+          <input type="password" name="password" className="input-box"  value={password}  onChange={handleChange} placeholder="Password" />
+          <input type="password" name="password_confirmation" className="input-box"  value={password_confirmation}  onChange={handleChange} placeholder="Confirm Password" />
+          <input type="submit" id="signup-btn" name="signup-btn" className="btn" value="Sign Up" />
+          <input type="button" id="signin-btn" name="signup-btn" className="btn" value="Already have an account?" />
+        </form>
+      </div>
     </div>
   );
 };
