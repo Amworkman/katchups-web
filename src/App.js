32ddc1fb93aae086d658592727911851adaadacf
Adam Workman <adammworkman25@gmail.com>
2021-01-31 16:33:19 -0500

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp'
+
 
 function App() {
   return (
     <div className="App">      
-        <Login/>
+      <Router>
+        <Route path="/login" component={Login} />
+        <Route path="/sign-up" component={SignUp} />
+        {/* <Route path="/" component={Main} /> */}
+      </Router>
     </div>
   );
 }
