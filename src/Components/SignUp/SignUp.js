b9b76fb95c03c97c4a10622202ea1c26a2816600
Adam Workman <adammworkman25@gmail.com>
2021-02-01 17:09:22 -0500

    Make sign in form functional

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import './SignUp.css';
 import {Link} from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../../Actions/LoginActions"
 
   const initialState = {
     name: "",
@@ -19,17 +21,25 @@ import {Link} from "react-router-dom"
 
   function SignUp() {
     const [state, dispatch] = useReducer(reducer, initialState);
-
+    const storeDispatch = useDispatch()
+    const user = useSelector(state => state.user)
+    
     const handleChange = (e) => {
       dispatch({name: e.target.name, value: e.target.value})
     } 
 
-    const { name, location, email, password, password_confirmation } = state  
+    const { name, location, email, password, password_confirmation } = state
+    
+    const handleSubmit = (e, state) =>{
+      e.preventDefault()
+      storeDispatch(userLogin(state, "users"))
+      debugger
+    }
 
   return (
     <div className="login-card">
       <div className="form">
-        <form>
+        <form onSubmit={(e) => handleSubmit(e, state, user)}>
           <input type="text" name="name" className="input-box" value={name} onChange={handleChange} placeholder="Full Name" />
           <input type="location" id="user-location-input" name="location" className="input-box"  value={location}  onChange={handleChange} placeholder="Location (ex. Atlanta, GA)" />
           <input type="email" name="email" className="input-box"  value={email}  onChange={handleChange} placeholder="Email" />
