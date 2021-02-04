6c38689d7d9705e1324a79d88b05ff0f9b297eeb
Adam Workman <adammworkman25@gmail.com>
2021-02-04 18:23:56 -0500

    Create RestaurantContainer

---
+++ src/Components/Main/Restaurants/RestaurantsContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './RestaurantsContainer.css'
+import { useEffect } from 'react';
+import { fetchRestaurants } from '../../../Actions/RestaurantActions'
+import { useDispatch, useSelector } from "react-redux";
+import Restaurant from './Restaurant';
+
+const RestaurantsContainer = () => {
+  const storeDispatch = useDispatch()
+  const restaurants = useSelector(state => state.restaurants)
+
+  useEffect(() => {
+    storeDispatch(fetchRestaurants())
+  }, [])
+
+  function renderRestaurants() {
+    return restaurants.map( restaurant => <
+      Restaurant 
+      key={restaurant.id}
+      name={restaurant.name}
+      price={restaurant.price}
+      rating={restaurant.rating}
+      location={restaurant.location}
+      categories={restaurant.categories}
+      img={restaurant.image_url}
+    />)
+  }
+
+  return (
+    <div className='left-container'>
+      <div className='restaurant-container'>
+        {renderRestaurants()}      
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantsContainer;
