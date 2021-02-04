aeda35d68f6ded93ec45cf9b9a9ef49feec20fbb
Adam Workman <adammworkman25@gmail.com>
2021-02-04 18:22:18 -0500

    Add text and return RestaurantsContainer

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import RestaurantsContainer from './Restaurants/RestaurantsContainer'
+import FriendsContainer from './Friends/FriendsContainer'
+import NavBar from './Nav/NavBar'
 
 const Main = () => {
   return (
     <div>
-      
+      <h1>katchups</h1>
+      <RestaurantsContainer />
     </div>
   );
 };
