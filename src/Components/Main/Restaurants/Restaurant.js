3ef44284909cf72de1e2e893f990c20cc1d4910e
Adam Workman <adammworkman25@gmail.com>
2021-02-04 18:23:17 -0500

    Create const Restaurant

---
+++ src/Components/Main/Restaurants/Restaurant.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Restaurant = (props) => {
+  return (
+    <div className="list">
+      <img className="list-img" src={props.img} /><br />
+      <h2>{props.name}</h2> <br/>
+      {props.categories.map(category => category.title).join(', ')}<br /> <br/>
+      {props.rating} stars   
+    </div>
+  );
+};
+
+export default Restaurant;
