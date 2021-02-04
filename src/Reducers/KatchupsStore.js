7d326a948dd6415fc6dd90dad236b3e92bf71492
Adam Workman <adammworkman25@gmail.com>
2021-02-04 18:24:47 -0500

    Add GET_RESTAURANTS to store

--- src/Reducers/KatchupsStore.js
+++ src/Reducers/KatchupsStore.js
@@ -1,20 +1,24 @@
 const KatchupsStore = ( state = {
-  user: "",
+  restaurants: [],
   loading: false
 }, action ) => {
   switch ( action.type ) {
     case 'LOADING':
-      debugger
       return {
         ...state,
         loading: true
       } 
     case 'USER_LOGIN':
-      debugger
       return {
         ...state,
         loading: false,
-      }  
+      } 
+    case 'GET_RESTAURANTS': 
+      return {
+        ...state,
+        restaurants: action.restaurants,
+        loading: false        
+      }
     default:
       return state; 
   }
