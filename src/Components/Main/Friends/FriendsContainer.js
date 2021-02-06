cfeee1216fee12d0ddf18611c4244a9df364385c
Adam Workman <adammworkman25@gmail.com>
2021-02-05 20:58:51 -0500

    Create FriendsContainer

---
+++ src/Components/Main/Friends/FriendsContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './FriendsContainer.css'
+import { useEffect } from 'react';
+import { fetchFriends } from '../../../Actions/FriendActions'
+import { useDispatch, useSelector } from "react-redux";
+import Friend from './Friend';
+
+const FriendsContainer = () => {
+  const storeDispatch = useDispatch()
+  const friends = useSelector(state => state.friends)
+
+  useEffect(() => {
+		storeDispatch(fetchFriends());
+	}, [storeDispatch]);
+
+  function renderFriends() {
+    return friends.map( friend => <
+      Friend
+        key={friend.id}
+        name={friend.name} 
+        img={friend.profile_img}
+        location={friend.location}
+        email={friend.email}
+    />)
+  }
+
+  return (
+    <div className='right-container'>
+      <div className='friend-container'>
+        {renderFriends()}
+      </div>
+    </div>
+  );
+};
+
+export default FriendsContainer;
