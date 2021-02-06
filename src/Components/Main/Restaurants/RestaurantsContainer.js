224d1a9dbb7dc64e8458561dae6e7232a5c59d93
Adam Workman <adammworkman25@gmail.com>
2021-02-06 18:32:54 -0500

    Import CSS

--- src/Components/Main/Restaurants/RestaurantsContainer.js
+++ src/Components/Main/Restaurants/RestaurantsContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./RestaurantsContainer.css";
+import './RestaurantsContainer.scoped.css';
 import { useEffect } from "react";
 import { fetchRestaurants } from "../../../Actions/RestaurantActions";
 import { useDispatch, useSelector } from "react-redux";
