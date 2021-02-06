61b80e533c6de1c3bd8c5be7f8afc38801effddc
Adam Workman <adammworkman25@gmail.com>
2021-02-05 20:58:17 -0500

    Create Friend component

---
+++ src/Components/Main/Friends/Friend.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friend = (props) => {
+  return (
+    <div>
+      <img className="list-img" src={props.img} /><br />
+      <h2>{props.name}</h2> <br /> <br /> <br />
+    </div>
+  );
+};
+
+export default Friend;
