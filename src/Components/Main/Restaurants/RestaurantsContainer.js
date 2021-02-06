e1fe3cea247bfb61cca83345c26158e2cd56a165
Adam Workman <adammworkman25@gmail.com>
2021-02-05 21:03:28 -0500

    Fix bug

--- src/Components/Main/Restaurants/RestaurantsContainer.js
+++ src/Components/Main/Restaurants/RestaurantsContainer.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import './RestaurantsContainer.css'
-import { useEffect } from 'react';
-import { fetchRestaurants } from '../../../Actions/RestaurantActions'
+import React from "react";
+import "./RestaurantsContainer.css";
+import { useEffect } from "react";
+import { fetchRestaurants } from "../../../Actions/RestaurantActions";
 import { useDispatch, useSelector } from "react-redux";
-import Restaurant from './Restaurant';
+import Restaurant from "./Restaurant";
 
 const RestaurantsContainer = () => {
-  const storeDispatch = useDispatch()
-  const restaurants = useSelector(state => state.restaurants)
+  
+	const storeDispatch = useDispatch();
+	const restaurants = useSelector((state) => state.restaurants);
+	useEffect(() => {
+		storeDispatch(fetchRestaurants());
+	}, [storeDispatch]);
 
-  useEffect(() => {
-    storeDispatch(fetchRestaurants())
-  }, [])
+	const renderRestaurants = () => {
+		return restaurants.map((restaurant) => ( <
+      Restaurant
+				key={restaurant.id}
+				name={restaurant.name}
+				price={restaurant.price}
+				rating={restaurant.rating}
+				location={restaurant.location}
+				categories={restaurant.categories}
+				img={restaurant.image_url}
+			/>
+		));
+	}
 
-  function renderRestaurants() {
-    return restaurants.map( restaurant => <
-      Restaurant 
-      key={restaurant.id}
-      name={restaurant.name}
-      price={restaurant.price}
-      rating={restaurant.rating}
-      location={restaurant.location}
-      categories={restaurant.categories}
-      img={restaurant.image_url}
-    />)
-  }
-
-  return (
-    <div className='left-container'>
-      <div className='restaurant-container'>
-        {renderRestaurants()}      
+	return (
+    
+		<div className="left-container">
+			<div className="restaurant-container">
+        {renderRestaurants()}
       </div>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default RestaurantsContainer;
