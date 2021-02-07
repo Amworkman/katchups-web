14228e3e1dd4fd8a653124927be4a3954371b375
Adam Workman <adammworkman25@gmail.com>
2021-02-07 16:27:21 -0500

    Add search to restaurants container

--- src/Components/Main/Restaurants/RestaurantsContainer.js
+++ src/Components/Main/Restaurants/RestaurantsContainer.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useReducer }  from 'react';
 import './RestaurantsContainer.scoped.css';
 import { useEffect } from "react";
 import { fetchRestaurants } from "../../../Actions/RestaurantActions";
 import { useDispatch, useSelector } from "react-redux";
 import Restaurant from "./Restaurant";
 
+const initialState = {
+  search: ""   
+}
+
+function reducer(state, { name, value }) {
+  return {
+    ...state,
+    [name]: value
+  }
+}
+
 const RestaurantsContainer = () => {
-  
+  const [state, dispatch] = useReducer(reducer, initialState);
 	const storeDispatch = useDispatch();
 	const restaurants = useSelector((state) => state.restaurants);
+	const parsedRestaurants = []
+
 	useEffect(() => {
 		storeDispatch(fetchRestaurants());
 	}, [storeDispatch]);
 
+	const handleChange = (e) => {
+    dispatch({name: e.target.name, value: e.target.value})
+  }
+
+  function searchRestaurantList(restaurant){
+    if (restaurant.name.toLowerCase().includes(state.search.toLowerCase())){
+      parsedRestaurants.push(restaurant)
+    }
+  }
+
 	const renderRestaurants = () => {
-		return restaurants.map((restaurant) => ( <
+		restaurants.filter(searchRestaurantList)
+		return parsedRestaurants.map((restaurant) => ( <
       Restaurant
 				key={restaurant.id}
 				name={restaurant.name}
@@ -30,6 +54,7 @@ const RestaurantsContainer = () => {
 	return (
     
 		<div className="left-container">
+			<input type="text" id="rest-search" name="search" className="search" onChange={handleChange} value={state.search} placeholder="Search Restaurants" />
 			<div className="box-fade box-fade-top">
 			</div>
 				<div className="restaurant-container">				
