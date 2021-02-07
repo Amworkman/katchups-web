f5ed1a06d6c6b757415f15fb3c1936488b7b7fa2
Adam Workman <adammworkman25@gmail.com>
2021-02-07 16:25:52 -0500

    Add search to friends

--- src/Components/Main/Friends/FriendsContainer.js
+++ src/Components/Main/Friends/FriendsContainer.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useReducer }  from 'react';
 import './FriendsContainer.scoped.css'
 import { useEffect } from 'react';
 import { fetchFriends } from '../../../Actions/FriendActions'
 import { useDispatch, useSelector } from "react-redux";
 import Friend from './Friend';
 
+const initialState = {
+  search: ""   
+}
+
+function reducer(state, { name, value }) {
+  return {
+    ...state,
+    [name]: value
+  }
+}
+
 const FriendsContainer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const storeDispatch = useDispatch()
   const friends = useSelector(state => state.friends)
+  const parsedFriends = []
 
   useEffect(() => {
-		storeDispatch(fetchFriends());
-	}, [storeDispatch]);
+    storeDispatch(fetchFriends());    
+  }, [storeDispatch])
+
+  const handleChange = (e) => {
+    dispatch({name: e.target.name, value: e.target.value})
+  }
+
+  function searchFriendList(friend){
+    if (friend.name.toLowerCase().includes(state.search.toLowerCase())){
+      parsedFriends.push(friend)
+    }
+  }
 
   function renderFriends() {
-    return friends.map( friend => <
+    friends.filter(searchFriendList)
+    return parsedFriends.map( friend => <
       Friend
         key={friend.id}
         name={friend.name} 
@@ -26,6 +50,7 @@ const FriendsContainer = () => {
 
   return (
     <div className='right-container'>
+      <input type="text" id="friend-search" name="search" className="search" value={state.search} onChange={handleChange} placeholder="Search Friends" />
       <div className="box-fade box-fade-top"></div>
       <div className='friend-container'>        
         {renderFriends()}
