fa811e152558c23761e38337c8f3d45c2b947edb
Adam Workman <adammworkman25@gmail.com>
2021-02-07 16:26:37 -0500

    Edit NavBar

--- src/Components/Main/Nav/NavBar.js
+++ src/Components/Main/Nav/NavBar.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './NavBar.scoped.css'
 
 const NavBar = () => {
+  const user = JSON.parse(localStorage.currentUser)
   return (
     <div className="navbar">
-      <input type="button" id="logout-btn" name="logout-btn" className="button button-logout" value="Log Out" />
-      {/* <img className=nav-img src=${user.profile_img} /> */}
-      <input type="button" id="user-tag" name="user-tag" className="button button-username" value="user name" />
-      <input type="button" id="find-btn" name="find-btn" className="button button-find" value="find friends" />
-      <input type="button" id="katchups-btn" name="katchups-btn" className="button button-recents" value="katchups" />
-      <input type="button" id="location-btn" name="location-btn" className="button button-location" value="change location" />     
+      <div className="middle">
+        <img className="button button-img" src={user.profile_img} />
+        <input type="button" id="logout-btn" name="logout-btn" className="button button-logout" value="Log Out" />        
+        <input type="button" id="user-tag" name="user-tag" className="button button-username" value={user.name} />
+        <input type="button" id="find-btn" name="find-btn" className="button button-find" value="find friends" />
+        <input type="button" id="katchups-btn" name="katchups-btn" className="button button-recents" value="katchups" />
+        <input type="button" id="location-btn" name="location-btn" className="button button-location" value={user.location} /> 
+      </div>    
     </div>
   );
 };
