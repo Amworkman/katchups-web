7efd73d4c5f48b585893f00dc7194595d7094d8e
Adam Workman <adammworkman25@gmail.com>
2021-02-08 12:54:24 -0500

    Add Menu to NavBar

--- src/Components/Main/Nav/NavBar.js
+++ src/Components/Main/Nav/NavBar.js
@@ -6,8 +6,13 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <div className="middle">
-        <img className="button button-img" src={user.profile_img} />
-        <input type="button" id="logout-btn" name="logout-btn" className="button button-logout" value="Log Out" />        
+        <div className="burger-menu">
+          <img className="button button-burger" src='burger.png' /> 
+          <input type="button" id="logout-btn" name="logout-btn" className="button menu-button logout" value="logout" />
+          <input type="button" id="settings-btn" name="settings-btn" className="button menu-button settings" value="settings" />
+          <input type="button" id="dark-btn" name="dark-btn" className="button menu-button dark" value="dark mode: off" />
+        </div>
+        <img className="button button-img" src={user.profile_img} />               
         <input type="button" id="user-tag" name="user-tag" className="button button-username" value={user.name} />
         <input type="button" id="find-btn" name="find-btn" className="button button-find" value="find friends" />
         <input type="button" id="katchups-btn" name="katchups-btn" className="button button-recents" value="katchups" />
