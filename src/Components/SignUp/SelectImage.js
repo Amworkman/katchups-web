7673fab83641b55b196171c4760276bbbc4cbb88
Adam Workman <adammworkman25@gmail.com>
2021-02-10 15:14:52 -0500

    Add default image to image selector

--- src/Components/SignUp/SelectImage.js
+++ src/Components/SignUp/SelectImage.js
@@ -4,23 +4,30 @@ import { updateUser } from "../../Actions/UserActions"
 import './SignUp.scoped.css';
 
 const SelectImage = () => {
-  const initialState = {
-    userImg: ""
-  }
 
   const user = JSON.parse(localStorage.currentUser)
-  const [userImg, setUserImg] = useState(initialState.userImg);
+  const [userImg, setUserImg] = useState({preview: "", raw: ""});
+  const [imageMessage, setImageMessage] = useState({message: "Choose a profile image"});
   const storeDispatch = useDispatch()
   const storeState = useSelector(state => state)
 
   const handleChange = (e) => {
-    setUserImg(e.target.files)
+    if (e.target.files.length) {
+      setUserImg({
+        preview: URL.createObjectURL(e.target.files[0]),
+        raw: e.target.files[0]
+      });
+    }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    if (userImg.preview !== ""){
     const formData = new FormData(e.target);
     storeDispatch(updateUser(user, formData))
+    } else {
+      setImageMessage({message: "Click on the katchups logo to upload a profile image"})     
+    }
   }
 
     if (storeState.updated === true){
@@ -28,11 +35,21 @@ const SelectImage = () => {
     }
 
   return (
-    <div className="signup-card">
-      <img className="profile-img" name="profile_img" src={user.profile_img}/>
+    <div className="signup-card">      
       <form onSubmit={handleSubmit} >
-        <input className="file-select" name="profile_img" type="file" accept="image/*" multiple="false" onChange={handleChange}/>
-        <input type="submit" value="submit"/>
+        <label htmlFor="profile_img">
+          <div className="image-mask">
+            upload photo
+          </div>
+        {userImg.preview ? (
+          <img src={userImg.preview} alt="dummy" className="profile-img" name="profile_img" />
+        ) : (
+          <img className="profile-img" name="profile_img" alt="dummy" src='/katchup.png' />
+        )}
+        </label>
+        <h2>{imageMessage.message}</h2>
+        <input style={{display: 'none'}} className="file-select" name="profile_img" id="profile_img" type="file" accept="image/*" multiple="false" onChange={handleChange}/>
+        <input type="submit" className="submit-btn" value="CREATE USER"/>
       </form>
     </div>
   );
