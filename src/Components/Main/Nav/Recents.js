575a06abc42da4e5b82f2e6a8de5f3e66ebd7ffd
Adam Workman <adammworkman25@gmail.com>
2021-02-14 17:04:33 -0500

    Create recnts component

---
+++ src/Components/Main/Nav/Recents.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.scoped.css'
+
+const Recents = () => {
+  return (
+    <>
+      <input type="button" id="katchups-btn" name="katchups-btn" className="button button-recents" value="katchups" />
+    </>
+  );
+};
+
+export default Recents;
