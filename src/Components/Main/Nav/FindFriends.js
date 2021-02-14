3687e10eed346345330504af7b7bb24b0903f726
Adam Workman <adammworkman25@gmail.com>
2021-02-14 17:03:04 -0500

    Create findFriends component

---
+++ src/Components/Main/Nav/FindFriends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.scoped.css'
+
+const FindFriends = () => {
+  return (
+    <>
+      <input type="button" id="find-btn" name="find-btn" className="button button-find" value="find friends" />
+    </>
+  );
+};
+
+export default FindFriends;
