1114d36f0e5a42e2cdf1e1b96d7aea911a4caaab
Adam Workman <adammworkman25@gmail.com>
2021-02-18 16:19:22 -0500

    Clear input on focus

--- src/Components/Main/Nav/Location.js
+++ src/Components/Main/Nav/Location.js
@@ -17,7 +17,7 @@ const Location = () => {
   
   let initialState = {
     currentLocation: user.location,
-    locationLength: user.location.length + "ch"
+    locationLength: user.location.length + 1 + "ch"
   } 
   
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -30,16 +30,20 @@ const Location = () => {
      storeDispatch(fetchRestaurants(currentLocation))
     }
 
+    const handleClearField = (e) => {
+      dispatch({name: e.target.name, value: ""})
+    }
+
   const handleChange = (e) => {
     dispatch({name: e.target.name, value: e.target.value})
     if (currentLocation.length > 2){
-      dispatch({name: e.target.id, value: e.target.value.length + 2 + "ch"})
-    } else { dispatch({name: e.target.id, value: e.target.placeholder.length + 1 + "ch"})  }
+      dispatch({name: e.target.id, value: e.target.value.length + 4 + "ch"})
+    } else { dispatch({name: e.target.id, value: e.target.placeholder.length + 2 + "ch"})  }
   }
   
   return (
     <>
-      <form onSubmit={handleSubmit}><input type="text" style={{width: locationLength}} id="locationLength" name="currentLocation"  className="button button-location"  onChange={handleChange.bind(this)} placeholder="ENTER CITY" value={currentLocation}></input></form>
+      <form onSubmit={handleSubmit}><input type="text" style={{width: locationLength}} id="locationLength" name="currentLocation"  className="button button-location"  onChange={handleChange.bind(this)} onFocus={handleClearField} placeholder="ENTER CITY" value={currentLocation}></input></form>
     </>
   );
 };
