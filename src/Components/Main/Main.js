851034732bd94bb85435fa9af191078de978b1b9
Adam Workman <adammworkman25@gmail.com>
2021-02-21 17:24:50 -0500

    Create outside click event

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useReducer, useEffect, useRef } from 'react';
 import RestaurantsContainer from './Restaurants/RestaurantsContainer'
 import FriendsContainer from './Friends/FriendsContainer'
 import NavBar from './Nav/NavBar'
+import { ReactFitty } from "react-fitty"
+import FriendCard from "./Friends/FriendCard"
+
+const initialState = {
+  search: '',
+  friendCard: ''  
+}
+
+function reducer(state, { name, value }) {
+  return {
+    ...state,
+    [name]: value
+  }
+}
+
+
 
 const Main = () => {
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+              dispatch({name: 'friendCard', value: ''})
+              let fitText = document.getElementById("fitText")
+              if (fitText.innerText !== "katchups"){
+                fitText.innerText = "katchups" 
+              }             
+            }
+        }
+  
+        document.addEventListener("mouseup", handleClickOutside);
+        return () => {
+            document.removeEventListener("mouseup", handleClickOutside);
+        };
+    }, [ref]);
+  }
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSelectedFriend = (props) => {
+    dispatch({name: 'friendCard', value: <FriendCard friend={props}/>})
+  }
   return (
-    <div>
-      <h1>katchups</h1>
+    <div className="main" >
+      <ReactFitty className="fitText" id="fitText" style={{opacity: 1}} >katchups</ReactFitty>      
       <RestaurantsContainer />
-      <FriendsContainer />
+      <FriendsContainer handleSelectedFriend={handleSelectedFriend}/>
+      <div ref={wrapperRef}>
+        {state.friendCard}
+      </div>
       <NavBar />
     </div>
   );
