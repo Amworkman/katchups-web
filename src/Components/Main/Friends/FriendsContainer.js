bfe625bd094fce4f58e8ae645fce59063e2d51ab
Adam Workman <adammworkman25@gmail.com>
2021-02-21 17:27:38 -0500

    Add handleSelectedFriend to Friend props

--- src/Components/Main/Friends/FriendsContainer.js
+++ src/Components/Main/Friends/FriendsContainer.js
@@ -4,9 +4,11 @@ import { useEffect } from 'react';
 import { fetchFriends } from '../../../Actions/FriendActions'
 import { useDispatch, useSelector } from "react-redux";
 import Friend from './Friend';
+import FriendCard from './FriendCard'
 
 const initialState = {
-  search: ""   
+  search: '',
+  friendCard: ''  
 }
 
 function reducer(state, { name, value }) {
@@ -16,7 +18,7 @@ function reducer(state, { name, value }) {
   }
 }
 
-const FriendsContainer = () => {
+const FriendsContainer = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const storeDispatch = useDispatch()
   const friends = useSelector(state => state.friends)
@@ -30,30 +32,35 @@ const FriendsContainer = () => {
     dispatch({name: e.target.name, value: e.target.value})
   }
 
-  function searchFriendList(friend){
+  const searchFriendList = (friend) => {
     if (friend.name.toLowerCase().includes(state.search.toLowerCase())){
       parsedFriends.push(friend)
     }
   }
 
-  function renderFriends() {
+  const renderFriends = (props) => {
     friends.filter(searchFriendList)
     return parsedFriends.map( friend => <
       Friend
         key={friend.id}
         name={friend.name} 
-        img={friend.profile_img}
+        img={friend.profile_img_url}
         location={friend.location}
         email={friend.email}
+        handleSelectedFriend={props.handleSelectedFriend}
     />)
   }
 
+  
+
   return (
+    
     <div className='right-container'>
       <input type="text" id="friend-search" name="search" className="search" value={state.search} onChange={handleChange} placeholder="Search Friends" />
       <div className="box-fade box-fade-top"></div>
       <div className='friend-container'>        
-        {renderFriends()}
+        {renderFriends(props)}
+        {state.friendCard}    
       </div>
       <div className="box-fade box-fade-bottom"></div>
     </div>
