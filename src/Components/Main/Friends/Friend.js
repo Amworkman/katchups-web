41a6f8e4b049d79605051f163797bdc9221fe425
Adam Workman <adammworkman25@gmail.com>
2021-02-21 17:26:26 -0500

    Create handleSelectedFriend

--- src/Components/Main/Friends/Friend.js
+++ src/Components/Main/Friends/Friend.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React  from 'react';
 import './FriendsContainer.scoped.css'
 
-const Friend = (props) => {
+
+
+const Friend = (props) => {  
+  const handleSelectedFriend = (props) => {
+    props.handleSelectedFriend(props)
+  }
+
   return (
     <div>
-      <img className="list-img" src={props.img} /><br />
+      <img  onClick={() => handleSelectedFriend(props)} className="list-img" src={props.img} /><br />
       <h2>{props.name}</h2> <br /> 
     </div>
   );
