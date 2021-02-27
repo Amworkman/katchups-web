e37d11957d5111a1c0f8fdb1f36f4b93c0fb3598
Adam Workman <adammworkman25@gmail.com>
2021-02-26 19:32:54 -0500

    Modify Restaraunt to include props

--- src/Components/Main/Restaurants/Restaurant.js
+++ src/Components/Main/Restaurants/Restaurant.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './RestaurantsContainer.scoped.css';
 
 const Restaurant = (props) => {
+  const handleSelectedRestaurant = (props) => {
+    props.handleSelectedRestaurant(props)
+  }
+
   return (
     <div className="list">
-      <img className="list-img" src={props.img} /><br />
+      <img className="list-img" onClick={() => handleSelectedRestaurant(props)} src={props.img} /><br />
       <h2>{props.name}</h2> <br/>
       {props.categories.map(category => category.title).join(', ')}<br /> <br/>
       {props.rating} stars   
