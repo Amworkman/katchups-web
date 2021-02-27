f2a6f44150a25e911c5a59d785dd6dddf76c9835
Adam Workman <adammworkman25@gmail.com>
2021-02-26 19:33:12 -0500

    Build RestaurantCard

---
+++ src/Components/Main/Restaurants/RestaurantCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './RestaurantsContainer.scoped.css'
+
+
+
+const RestaurantCard = (props) => {
+  let fitText = document.getElementById("fitText")
+  
+  const changeText = () => {
+    fitText.innerText = props.restaurant.name
+  }
+
+  return (
+    <>
+      {changeText()}
+      <div className="restaurantImg-outer">
+        <div className="restaurantImg-inner">
+          <img  className="restaurantImg" src={props.restaurant.img} /><br />
+        </div>
+      </div>
+      <div className="card">
+        <div className="cardInner">
+          <div className="recents">
+            no recent katchups at {props.restaurant.name}
+          </div>
+          <button className="katchupButton"> katchup here </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RestaurantCard;
