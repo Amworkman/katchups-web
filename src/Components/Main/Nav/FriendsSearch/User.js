dd9ef479fb11ddc2eee7d9dbca3aee32df5744b6
Adam Workman <adammworkman25@gmail.com>
2021-04-06 19:46:41 -0400

    Add inner and outer listImg

--- src/Components/Main/Nav/FriendsSearch/User.js
+++ src/Components/Main/Nav/FriendsSearch/User.js
@@ -4,8 +4,12 @@ import '../NavBar.scoped.css'
 const User = (props) => {
   return (
     <div className="user">
-      <img className="list-img" src={props.img} /><br />
-      <h2>{props.name}</h2> <br /> 
+      <div className="listImg-outer">
+        <div className="listImg-inner">
+          <img className="list-img" src={props.img} /><br />
+          <h2>{props.name}</h2> <br /> 
+        </div>
+      </div>
     </div>
   );
 };
