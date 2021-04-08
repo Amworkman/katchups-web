17d287f4abe6b42b474f988265c9359e7e38c13f
Adam Workman <adammworkman25@gmail.com>
2021-04-07 21:18:19 -0400

    Add ability to select friend

--- src/Components/Main/Friends/Friend.js
+++ src/Components/Main/Friends/Friend.js
@@ -6,16 +6,16 @@ import './FriendsContainer.scoped.css'
 const Friend = (props) => {  
   const handleSelectedFriend = (props) => {
     props.handleSelectedFriend(props)
+    document.getElementById(`friend${props.id}`).className = "listImg-outer--selected"
   }
 
   return (
     <div>
-      <div className="listImg-outer">
-        <div className="listImg-inner">
-          <img  onClick={() => handleSelectedFriend(props)} className="list-img" src={props.img} /><br />
-        </div>
+      <div className="listImg-outer" id={`friend${props.id}`}>
+        <div onClick={() => handleSelectedFriend(props)} className="listImg-inner"></div>
+        <img className="list-img" src={props.img} /><br />        
       </div>
-      <h2>{props.name}</h2> <br /> 
+      <h2>{props.name}</h2> 
     </div>
   );
 };
