f46d9c9a065547f9d88b8df80b7fc33d4f5d8edf
Adam Workman <adammworkman25@gmail.com>
2021-04-07 21:21:20 -0400

    Refactor for new styling

--- src/Components/Main/Restaurants/Restaurant.js
+++ src/Components/Main/Restaurants/Restaurant.js
@@ -4,14 +4,14 @@ import './RestaurantsContainer.scoped.css';
 const Restaurant = (props) => {
   const handleSelectedRestaurant = (props) => {
     props.handleSelectedRestaurant(props)
+    document.getElementById(`restaurant${props.id}`).className = "listImg-outer--selected"
   }
-
+  
   return (
-    <div className="list">
-      <div className="listImg-outer">
-        <div className="listImg-inner">
-          <img className="list-img" onClick={() => handleSelectedRestaurant(props)} src={props.img} /><br />
-        </div>
+    <div>
+      <div className="listImg-outer" id={`restaurant${props.id}`}>
+        <div onClick={() => handleSelectedRestaurant(props)} className="listImg-inner"></div>
+        <img className="list-img" src={props.img} /><br />        
       </div>
       <h2>{props.name}</h2> <br/>
       {props.categories.map(category => category.title).join(', ')}<br /> <br/>
