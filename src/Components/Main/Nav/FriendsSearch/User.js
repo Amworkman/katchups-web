bae9fe4d6b2e987660b74c3898aa256589967619
Adam Workman <adammworkman25@gmail.com>
2021-04-07 21:20:40 -0400

    Clean up

--- src/Components/Main/Nav/FriendsSearch/User.js
+++ src/Components/Main/Nav/FriendsSearch/User.js
@@ -5,11 +5,10 @@ const User = (props) => {
   return (
     <div className="user">
       <div className="listImg-outer">
-        <div className="listImg-inner">
-          <img className="list-img" src={props.img} /><br />
-          <h2>{props.name}</h2> <br /> 
-        </div>
+        <div className="listImg-inner"></div> <br />
+        <img className="list-img" src={props.img} /><br />     
       </div>
+      <h2>{props.name}</h2>  
     </div>
   );
 };
