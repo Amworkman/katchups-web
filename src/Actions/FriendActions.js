29e5fcd934f86547d1ea52dfb0839e6b2b81d909
Adam Workman <adammworkman25@gmail.com>
2021-04-09 04:15:37 -0400

    Add fetchFriendRequests

--- src/Actions/FriendActions.js
+++ src/Actions/FriendActions.js
@@ -15,3 +15,21 @@ export const fetchFriends = () => {
     })
   }
 }
+
+export const fetchFriendRequests = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_PENDING'})
+    fetch(`http://localhost:3000/friends/requests`,{
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token")
+      }
+    })
+    .then(resp => {
+      return resp.json()
+    }).then(data => {
+      dispatch({ type: 'GET_FRIEND_REQUESTS', friendRequests: data })
+    })
+  }
+}
