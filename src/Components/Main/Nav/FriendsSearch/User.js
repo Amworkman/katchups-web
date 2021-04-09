aed4c9c991d1427b14e736f94d2b288bf9196d87
Adam Workman <adammworkman25@gmail.com>
2021-04-08 22:08:53 -0400

    Add handleSelectedFriend

--- src/Components/Main/Nav/FriendsSearch/User.js
+++ src/Components/Main/Nav/FriendsSearch/User.js
@@ -2,10 +2,15 @@ import React from 'react';
 import '../NavBar.scoped.css'
 
 const User = (props) => {
+  const handleSelectedFriend = (props) => {
+    props.handleSelectedFriend(props)
+    props.handleBlur()
+  }
+
   return (
     <div className="user">
       <div className="listImg-outer">
-        <div className="listImg-inner"></div> <br />
+        <div className="listImg-inner" onClick={() => handleSelectedFriend(props)}></div> <br />
         <img className="list-img" src={props.img} /><br />     
       </div>
       <h2>{props.name}</h2>  
