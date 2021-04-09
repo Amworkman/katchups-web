762a3ebea595fad6495e245c4c85d999d3cc763f
Adam Workman <adammworkman25@gmail.com>
2021-04-09 04:17:08 -0400

    Edit className of outer

--- src/Components/Main/Friends/Friend.js
+++ src/Components/Main/Friends/Friend.js
@@ -4,6 +4,7 @@ import './FriendsContainer.scoped.css'
 
 
 const Friend = (props) => {  
+
   const handleSelectedFriend = (props) => {
     props.handleSelectedFriend(props)
     document.getElementById(`friend${props.id}`).className = "listImg-outer--selected"
@@ -11,7 +12,7 @@ const Friend = (props) => {
 
   return (
     <div>
-      <div className="listImg-outer" id={`friend${props.id}`}>
+      <div className={props.status + " listImg-outer"} id={`friend${props.id}`}>
         <div onClick={() => handleSelectedFriend(props)} className="listImg-inner"></div>
         <img className="list-img" src={props.img} /><br />        
       </div>
