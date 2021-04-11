eec9574428f40edfcb6671b2a060e4639d7bf437
Adam Workman <adammworkman25@gmail.com>
2021-04-11 19:33:44 -0400

    Clean up

--- src/Components/Main/Friends/FriendCard.js
+++ src/Components/Main/Friends/FriendCard.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserFriends, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from "react-redux"
 import { friendRequest } from "../../../Actions/UserActions"
-import { acceptFriendRequest, rejectFriendRequest, fetchFriends, fetchFriendRequests } from "../../../Actions/FriendActions"
+import { acceptFriendRequest, rejectFriendRequest } from "../../../Actions/FriendActions"
 
 const initialState = {
   acceptButton: "",
@@ -21,9 +21,6 @@ function reducer(state, { name, value }) {
 const FriendCard = (props) => {
   const userID = JSON.parse(localStorage.currentUser).id
   const friendID = props.friend.id
-  const listFriend = document.getElementById(`listFriend${friendID}`)
-
-  
 
   const [state, dispatch] = useReducer(reducer, initialState);
   let fitText = document.getElementById("fitText")
@@ -38,23 +35,21 @@ const FriendCard = (props) => {
       dispatch({name: 'acceptButton', value: <button className="acceptButton" onClick={handleAccept}> <FontAwesomeIcon icon={faCheck} /> </button>})
       dispatch({name: 'rejectButton', value: <button className="rejectButton" onClick={handleReject}> <FontAwesomeIcon icon={faTimes} /> </button>})
     }
-	}, [storeDispatch]);
+	});
 
   const handleAccept = () => {
     dispatch({name: 'acceptButton', value: ""})
-    dispatch({name: 'rejectButton', value:""}) 
-    storeDispatch(acceptFriendRequest(userID, friendID))    
-    storeDispatch(fetchFriends());
-    storeDispatch(fetchFriendRequests());    
-    props.friend.refreshFriendsList()
-  }
+    dispatch({name: 'rejectButton', value:""})
+    storeDispatch(acceptFriendRequest(userID, friendID)).then(() => {
+      props.friend.refreshFriendsList()
+    })  }
 
   const handleReject = () => {
     dispatch({name: 'acceptButton', value: ""})
     dispatch({name: 'rejectButton', value:""})
-    storeDispatch(rejectFriendRequest(userID, friendID))  
-    listFriend.remove()  
-  }
+    storeDispatch(rejectFriendRequest(userID, friendID)).then(() => {
+      props.friend.refreshFriendsList()
+    })  }
   
   const handleAddFriend = () => {
     storeDispatch(friendRequest(userID, friendID))
@@ -66,7 +61,7 @@ const FriendCard = (props) => {
 
   let button = <button className="katchupButton" onClick={handleKatchup}> katchup </button>
 
-  if (props.friend.confirmed == false){
+  if (props.friend.confirmed === false){
     button = <button className="katchupButton" style={{backgroundColor: "#5387e7"}} onClick={handleAddFriend}> <FontAwesomeIcon icon={faUserFriends} /> </button>
   }
 
@@ -75,7 +70,7 @@ const FriendCard = (props) => {
       {changeText()}
       <div className="friendImg-outer">
         <div className="friendImg-inner"></div>
-        <img  className="friendImg" src={props.friend.img} /><br />        
+        <img  className="friendImg" src={props.friend.img} alt="firend"/><br />        
       </div>
       <div className="card">
         <div className="cardInner">
@@ -85,8 +80,7 @@ const FriendCard = (props) => {
           <div className="cardInner-right">
             {button}
           </div>
-        </div>
-         
+        </div>         
       </div>
       {state.acceptButton}
       {state.rejectButton} 
