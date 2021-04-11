723beec6ec6364336c5ef0bc88b4106089af1881
Adam Workman <adammworkman25@gmail.com>
2021-04-11 19:32:20 -0400

    add compose

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import KatchupsStore from './Reducers/KatchupsStore';
 
-
-const store = createStore(KatchupsStore, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(KatchupsStore, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
