0119448a9a06c87e98855abf60eaa5f064807404
Adam Workman <adammworkman25@gmail.com>
2021-04-21 19:58:16 -0400

    Add restaurant info

--- src/Components/Main/Restaurants/RestaurantCard.js
+++ src/Components/Main/Restaurants/RestaurantCard.js
@@ -13,14 +13,34 @@ const RestaurantCard = (props) => {
   return (
     <>
       {changeText()}
+      
       <div className="restaurantImg-outer">
         <div className="restaurantImg-inner"></div>
         <img  className="restaurantImg" alt="" src={props.restaurant.img} /><br />        
       </div>
       <div className="card">
         <div className="cardInner">
-          <div className="recents">
-            no recent katchups at {props.restaurant.name}
+          <div className="restaurantInfo">
+            <h3>{props.restaurant.location.display_address[0]}</h3>
+            <h3>{props.restaurant.location.display_address[1]}</h3>
+            <h4>{props.restaurant.phone}</h4>
+            <div className="restaurantInfoInner">
+              <div>
+                <span className="price">{props.restaurant.price}</span>
+                <span className="emptyPrice">$$$$</span>
+              </div>  <br />        
+              <div className="hours">
+                <a href={props.restaurant.url} target="_blank" rel="noopener noreferrer">
+                  BUSINESS HOURS
+                </a>
+              </div>
+              <div className="reviews">
+                <span className="reviewText">{props.restaurant.reviewCount} reviews on </span>
+                <a href={props.restaurant.url} target="_blank" rel="noopener noreferrer">
+                  <img className="yelpImg" src="/yelp.png"/>
+                </a>
+              </div>
+            </div>
           </div>
           <div className="cardInner-right">
             <button className="katchupButton"> katchup here </button>
