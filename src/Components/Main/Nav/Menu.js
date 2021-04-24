8b1e32310d5a72529ed3222532e39470967f41e2
Adam Workman <adammworkman25@gmail.com>
2021-04-24 19:54:48 -0400

    Move click listener to burger button

--- src/Components/Main/Nav/Menu.js
+++ src/Components/Main/Nav/Menu.js
@@ -12,14 +12,16 @@ const Menu = () => {
   }
 
   const handleClick = () => {
+    debugger
     if (document.getElementsByClassName("burger-menu")[0]){
-      document.getElementsByClassName("burger-menu")[0].className = "burger-menu--selected"
+      debugger
+      document.getElementsByClassName("burger-menu")[0].classList.toggle("burger-menu--selected")
     }
   }
 
   return (
-    <div className="burger-menu" onClick={handleClick}>
-      <img className="button button-burger" src='burger.png' alt=""/> 
+    <div className="burger-menu">
+      <img className="button button-burger" onClick={handleClick} src='burger.png' alt=""/> 
       <input type="button" id="logout-btn" name="logout-btn" onClick={handleLogout} className="button menu-button logout" value="logout" />
       <input type="button" id="settings-btn" name="settings-btn" className="button menu-button settings" value="settings" />
       <input type="button" id="dark-btn" name="dark-btn" className="button menu-button dark" value="dark mode: off" />
