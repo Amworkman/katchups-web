323b32922f297b5e61b5e40d245c79622ced4c12
Adam Workman <adammworkman25@gmail.com>
2021-04-24 19:54:17 -0400

    Create DatePicker component

---
+++ src/Components/Main/Friends/Katchups/DatePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../FriendsContainer.scoped.css'
+
+
+const DatePicker = () => {
+  return (
+    <div className="datePicker">      
+       
+    </div>
+  );
+};
+
+export default DatePicker;
