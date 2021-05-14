704ec63d0048bcf4fa8003d8882a9e8ae72b403c
Adam Workman <adammworkman25@gmail.com>
2021-05-14 12:17:08 -0400

    Add ability to send friend request

--- src/Components/Main/Friends/InfoCard.js
+++ src/Components/Main/Friends/InfoCard.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useDispatch } from 'react-redux';
 import './FriendsContainer.scoped.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserFriends} from '@fortawesome/free-solid-svg-icons'
+import { friendRequest } from "../../../Actions/UserActions"
 
 const InfoCard = (props) => {
+  const storeDispatch = useDispatch()
+  const userID = JSON.parse(localStorage.currentUser).id
+  const friendID = props.friend.id
+
+  const handleAddFriend = () => {
+    storeDispatch(friendRequest(userID, friendID))
+  }
 
   let button = <button className="katchupButton" onClick={props.handleKatchup}> katchup </button>
+
+  if (props.friend.confirmed === false){
+    button = <button className="katchupButton" style={{backgroundColor: "#5387e7"}} onClick={handleAddFriend}> <FontAwesomeIcon icon={faUserFriends} /> </button>
+  }
 
   return (
     <>
