271f1270163facce99f121ac41c8e55dc75a01b3
Adam Workman <adammworkman25@gmail.com>
2021-05-27 15:22:26 -0400

    Configure DatePicker

--- src/Components/Main/Friends/Katchups/DatePicker.js
+++ src/Components/Main/Friends/Katchups/DatePicker.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MtrDatepicker } from "./mtr-datepicker.min.js"
+import "./datePicker.scoped.css"
+import Clock from "./Clock"
 
 const DatePicker = () => { 
 
-  let demo = new MtrDatepicker({
+  const datePicker = new MtrDatepicker({
     target: 'datepicker'
   })
 
-  return (    
-    <div className='datePicker'>         
+  const [time, setTime] = useState(datePicker.getFullTime());
+  const [date, setDate] = useState(datePicker.toDateString().slice(0, -5));
+
+  
+
+  // setInterval(() => {
+  //   setTime(datePicker.getFullTime())
+  //   setDate(datePicker.toDateString().slice(0, -5))
+  // }, 1000);
+
+  
+  return ( 
+    <div>
+      <div className='datePicker'>    
+      </div>      
+      <div className="dateBoxLeft">
+        <Clock/>        
+        <button className="button now" >katchup now</button>
+        <h2>or</h2>
+        <button className="button setDate">katchup at {time} on {date}</button>
+      </div>
+      <div className="dateBoxRight">
+      </div>
+      
     </div>  
   );
 };
