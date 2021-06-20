6e8926fbf42550fac4d494dec90c68a0969f92d2
Adam Workman <adammworkman25@gmail.com>
2021-06-20 16:55:00 -0400

    Create katchup card

---
+++ src/Components/Main/Friends/Katchups/katchup/KatchupCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./katchupCard.scoped.css"
+
+const KatchupCard = () => {
+  return (
+    <div className="katchupStripe">
+      
+      
+    </div>
+  );
+};
+
+export default KatchupCard;
