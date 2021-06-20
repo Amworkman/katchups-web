0ea2450fbde3cd3c09fe97dee0b97f06f1958de5
Adam Workman <adammworkman25@gmail.com>
2021-06-20 14:56:32 -0400

    Create katchup actions

---
+++ src/Actions/KatchupActions.js
@@ -0,0 +1,16 @@
+export const createKatchup = (friend, date, location) => {
+  return async(dispatch) => {
+    dispatch({ type: 'LOADING'})
+    await fetch(`http://localhost:3000/katchups`,{
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token")
+      },
+      body: JSON.stringify({user_id: JSON.parse(localStorage.currentUser).id, friend_id: friend, starts_at: date, location: location}),
+    })
+    .then(resp => {
+      return resp.json()
+    })
+  }
+}
